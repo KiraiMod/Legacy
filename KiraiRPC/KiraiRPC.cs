d500dc9e3dd14941c244c8961413edde420b3915
xKiraiChan <xKiraiChan@gmail.com>
2022-04-20 20:40:33 -0400

    sdk3 support

--- KiraiRPC/KiraiRPC.cs
+++ KiraiRPC/KiraiRPC.cs
@@ -21,6 +21,11 @@ namespace KiraiMod
     public class KiraiRPC : MelonMod
     {
         public static System.Action<RPCData> callbackChain = new System.Action<RPCData>((data) => { });
+        public static bool isSDK3;
+
+        private static VRC_EventHandler handler;
+        private object token;
+        private HarmonyInstance harmony;
 
         public static class Config
         {
@@ -48,10 +53,6 @@ namespace KiraiMod
             OnInit = 0x000,
         }
 
-        private static VRC_EventHandler handler;
-
-        private HarmonyInstance harmony;
-
         public override void OnApplicationStart()
         {
             harmony = HarmonyInstance.Create("KiraiRPC");
@@ -60,7 +61,7 @@ namespace KiraiMod
             MelonLogger.Log(System.ConsoleColor.Cyan, $"{new string('v', 26)}\n");
             MelonLogger.Log(System.ConsoleColor.Cyan, "Running a debug build");
             MelonLogger.Log(System.ConsoleColor.Cyan, "Upload logs to #crash-logs\n");
-            MelonLogger.Log(System.ConsoleColor.Cyan, $"{new string('^', 26)}\n");
+            MelonLogger.Log(System.ConsoleColor.Cyan, $"{new string('^', 26)}");
 #endif
         }
 
@@ -68,15 +69,20 @@ namespace KiraiMod
         {
             if (level != -1) return;
             handler = null;
-            MelonCoroutines.Start(WaitForLevelToLoad());
+
+            if (token != null) MelonCoroutines.Stop(token);
+
+            token = MelonCoroutines.Start(WaitForLevelToLoad());
         }
 
         private System.Collections.IEnumerator WaitForLevelToLoad()
         {
-            int sleep = 0;           
+            int sleep = 0;
 
-            while ((VRCPlayer.field_Internal_Static_VRCPlayer_0 == null || 
-                (handler = Object.FindObjectOfType<VRC_EventHandler>()) == null) && 
+            isSDK3 = Object.FindObjectOfType<VRCSDK2.VRC_SceneDescriptor>() is null;
+
+            while ((VRCPlayer.field_Internal_Static_VRCPlayer_0 == null ||
+                (handler = Object.FindObjectOfType<VRC_EventHandler>()) == null) &&
                 sleep < 60)
             {
                 sleep++;
@@ -85,28 +91,30 @@ namespace KiraiMod
 
             if (sleep >= 60)
             {
-                MelonLogger.Log("Abandoning RPC system, world is probably SDK3.");
                 yield break;
             }
+            token = null;
+
 #if DEBUG
             MelonLogger.Log($"Found Event listener after {sleep} seconds");
 #endif
+
             SendRPC(0xD00);
         }
 
         private System.Collections.IEnumerator Initialize()
         {
-            while (NetworkManager.field_Internal_Static_NetworkManager_0 is null) yield return null;
-
             try
             {
                 harmony.Patch(typeof(VRC_EventDispatcherRFC)
                     .GetMethod(nameof(VRC_EventDispatcherRFC.Method_Public_Void_Player_VrcEvent_VrcBroadcastType_Int32_Single_0), BindingFlags.Public | BindingFlags.Instance),
                     new HarmonyMethod(typeof(KiraiRPC).GetMethod(nameof(OnRPC), BindingFlags.NonPublic | BindingFlags.Static)));
 
-                MelonLogger.Log("Hooking RPCs... Passed");
+                LogWithPadding("OnRPC", true);
             }
-            catch { MelonLogger.LogWarning("Hooking RPCs... Failed"); }
+            catch { LogWithPadding("OnRPC", false); }
+
+            yield break;
         }
 
         public static System.Action<int, string> GetSendRPC(string name)
@@ -121,52 +129,64 @@ namespace KiraiMod
         {
             if (__0?.field_Private_APIUser_0 is null || __1 is null) return;
 
+            if (__1.EventType == VrcEventType.SendRPC && __1.ParameterString == "UdonSyncRunProgramAsRPC")
+            {
+                ProcessRPC(__0, System.Text.Encoding.UTF8.GetString(__1.ParameterBytes).Substring(6));
+            }
+
             if (__1.EventType == VrcEventType.ActivateCustomTrigger)
             {
-                if (__1.ParameterString.Length < 1) return;
-                if (__1.ParameterString[0] == 'k')
-                {
+                ProcessRPC(__0, __1.ParameterString);
+            }
+        }
+
+        private static void ProcessRPC(Player player, string rpc)
+        {
+            if (rpc.Length < 1) return;
+            if (rpc[0] == 'k')
+            {
 #if DEBUG
-                    MelonLogger.Log($"Recieved {__1.ParameterString}");
+                MelonLogger.Log($"Recieved {rpc}");
 #endif
 
-                    if (__1.ParameterString.Length < 5)
-                    {
-                        MelonLogger.Log($"{__0.field_Private_APIUser_0.displayName} sent a malformed kRPC (invalid size).");
-                        return;
-                    }
-
-                    string sid = __1.ParameterString.Substring(1, 3);
-                    if (!int.TryParse(sid, System.Globalization.NumberStyles.HexNumber, null, out int id)) {
-                        MelonLogger.Log($"{__0.field_Private_APIUser_0.displayName} sent a malformed kRPC (invalid id).");
-                        return;
-                    }
-
-                    string slen = __1.ParameterString.Substring(4, 1);
-                    if (!int.TryParse(slen, System.Globalization.NumberStyles.HexNumber, null, out int len))
-                    {
-                        MelonLogger.Log($"{__0.field_Private_APIUser_0.displayName} sent a malformed kRPC (invalid length).");
-                        return;
-                    }
-
-                    string target = __1.ParameterString.SafeSubstring(5, len);
-                    string payload = __1.ParameterString.SafeSubstring(5 + len);
-
-                    if (len == 0) // intended for us
-                    {
-                        switch (id)
-                        {
-                            case 0xD00:
-                                if (callbackChain != null)
-                                    callbackChain.Invoke(new RPCData("KiraiRPC", (int)RPCEventIDs.OnInit, __0.field_Private_APIUser_0.displayName, ""));
-                                break;
-                        }
-                    }
-
-                    if (callbackChain != null) 
-                        callbackChain.Invoke(new RPCData(len == 0 ? "KiraiRPC" : target, id,__0.field_Private_APIUser_0.displayName, payload));
+                if (rpc.Length < 5)
+                {
+                    MelonLogger.Log($"{player.field_Private_APIUser_0.displayName} sent a malformed kRPC (invalid size).");
+                    return;
                 }
+
+                string sid = rpc.Substring(1, 3);
+                if (!int.TryParse(sid, System.Globalization.NumberStyles.HexNumber, null, out int id))
+                {
+                    MelonLogger.Log($"{player.field_Private_APIUser_0.displayName} sent a malformed kRPC (invalid id).");
+                    return;
+                }
+
+                string slen = rpc.Substring(4, 1);
+                if (!int.TryParse(slen, System.Globalization.NumberStyles.HexNumber, null, out int len))
+                {
+                    MelonLogger.Log($"{player.field_Private_APIUser_0.displayName} sent a malformed kRPC (invalid length).");
+                    return;
+                }
+
+                string target = rpc.SafeSubstring(5, len);
+                string payload = rpc.SafeSubstring(5 + len);
+
+                if (len == 0) // intended for us
+                {
+                    switch (id)
+                    {
+                        case 0xD00:
+                            if (callbackChain != null)
+                                callbackChain.Invoke(new RPCData("KiraiRPC", (int)RPCEventIDs.OnInit, player.field_Private_APIUser_0.displayName, ""));
+                            break;
+                    }
+                }
+
+                if (callbackChain != null)
+                    callbackChain.Invoke(new RPCData(len == 0 ? "KiraiRPC" : target, id, player.field_Private_APIUser_0.displayName, payload));
             }
+
         }
 
         private static void SendRPC(int id)
@@ -216,19 +236,45 @@ namespace KiraiMod
                 return;
             }
 
-            handler.TriggerEvent(
-            new VrcEvent
+            if (isSDK3)
             {
-                EventType = VrcEventType.ActivateCustomTrigger,
-                Name = "",
-                ParameterObject = handler.gameObject,
-                ParameterInt = 0,
-                ParameterFloat = 0f,
-                ParameterString = raw,
-                ParameterBoolOp = VrcBooleanOp.Unused,
-                ParameterBytes = new Il2CppStructArray<byte>(0L)
-            },
-            VrcBroadcastType.AlwaysUnbuffered, VRCPlayer.field_Internal_Static_VRCPlayer_0.gameObject, 0f);
+                handler.TriggerEvent(
+                new VrcEvent
+                {
+                    EventType = VrcEventType.SendRPC,
+                    Name = "SendRPC",
+                    ParameterObject = handler.gameObject,
+                    ParameterInt = Player.prop_Player_0.field_Private_VRCPlayerApi_0.playerId,
+                    ParameterFloat = 0f,
+                    ParameterString = "UdonSyncRunProgramAsRPC",
+                    ParameterBoolOp = VrcBooleanOp.Unused,
+                    ParameterBytes = Networking.EncodeParameters(new Il2CppSystem.Object[] {
+                        raw
+                    })
+                },
+                VrcBroadcastType.AlwaysUnbuffered, VRCPlayer.field_Internal_Static_VRCPlayer_0.gameObject, 0f);
+            }
+            else
+            {
+                handler.TriggerEvent(
+                new VrcEvent
+                {
+                    EventType = VrcEventType.ActivateCustomTrigger,
+                    Name = "",
+                    ParameterObject = handler.gameObject,
+                    ParameterInt = 0,
+                    ParameterFloat = 0f,
+                    ParameterString = raw,
+                    ParameterBoolOp = VrcBooleanOp.Unused,
+                    ParameterBytes = new Il2CppStructArray<byte>(0L)
+                },
+                VrcBroadcastType.AlwaysUnbuffered, VRCPlayer.field_Internal_Static_VRCPlayer_0.gameObject, 0f);
+            }
+        }
+
+        private static void LogWithPadding(string src, bool passed)
+        {
+            MelonLogger.Log($"Hooking {src}...".PadRight(73, ' ') + (passed ? "Passed" : "Failed"));
         }
     }
 }
