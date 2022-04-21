f33ae2730e851055372aa43bd8a1a9823ba50bf4
xKiraiChan <xKiraiChan@gmail.com>
2022-04-20 20:40:50 -0400

    remove old build targets and add new empty project

---
+++ KiraiLog/KiraiLog.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace KiraiMod
+{
+    public class KiraiLog : MelonMod
+    {
+        static KiraiLog()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(
+#if DEBUG
+                "KiraiMod.Lib.KiraiLib.dll"
+#else
+                "KiraiMod.Lib.KiraiLibLoader.dll"
+#endif
+                );
+
+            MemoryStream mem = new MemoryStream((int)stream.Length);
+            stream.CopyTo(mem);
+
+            Assembly.Load(mem.ToArray());
+
+            new Action(() =>
+#if DEBUG
+                KiraiLib.NoOp()
+#else
+                KiraiLibLoader.Load()
+#endif
+            )();
+        }
+    }
+}
