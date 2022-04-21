ecea33d3f1a5516e6526313c340683d540f7d550
xKiraiChan <xKiraiChan@gmail.com>
2022-04-20 20:40:36 -0400

    fix exception in hook

--- KiraiMod/Hooks.cs
+++ KiraiMod/Hooks.cs
@@ -307,7 +307,7 @@ namespace KiraiMod
 
         private static bool HighlightRenderer(Renderer __0, bool __1)
         {
-            return !Shared.modules.esp.state || __1 || __0.name != "SelectRegion";
+            return !Shared.modules.esp.state || __1 || __0?.name != "SelectRegion";
         }
 
         private static void LogWithPadding(string src, bool passed)
