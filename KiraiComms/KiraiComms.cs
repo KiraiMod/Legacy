709c7e202229a62e143e85386afe7cf351458937
xKiraiChan <xKiraiChan@gmail.com>
2022-04-20 20:40:26 -0400

    basic skeleton for comms

---
+++ KiraiComms/KiraiComms.cs
@@ -0,0 +1,44 @@
+﻿using MelonLoader;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace KiraiMod
+{
+    public class KiraiComms : MelonMod
+    {
+        private System.Action<int, string> SendRPC;
+        private Dictionary<string, RSA> encryptors;
+        private RSA rsa;
+
+        public override void OnApplicationStart()
+        {
+            rsa = RSA.Create(4096);
+
+            if (MelonHandler.Mods.Any(m => m.Assembly.GetName().Name == "KiraiRPC"))
+            {
+                new System.Action(() =>
+                {
+                    SendRPC = KiraiRPC.GetSendRPC("KiraiComms");
+                    KiraiRPC.callbackChain += new System.Action<KiraiRPC.RPCData>((data) =>
+                    {
+                        if (data.target == "KiraiRPC")
+                        {
+                            if (data.id == (int)KiraiRPC.RPCEventIDs.OnInit)
+                            {
+
+                            }
+                        }
+                        else if (data.target == "KiraiComms")
+                        {
+                            switch (data.id)
+                            {
+
+                            }
+                        }
+                    });
+                }).Invoke();
+            }
+        }
+    }
+}
